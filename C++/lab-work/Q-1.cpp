49c0fb57aec989d3102d732ef04d45b7ce57486b
Rahul Patadiya <rahulpatadiya07@gmail.com>
2025-05-08 12:59:18 +0530

    Program 08-05-25

---
+++ C++/lab-work/Q-1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+   cout <<  "Name \t: Rahul Patadiya\n";
+   cout << "Age \t: 22\n";
+   cout << "School \t: SMt L.L.Kakadiya (sec) School\n";
+}
+    
---
+++ C++/lab-work/Q-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    cout << "----------\n";
+    cout << "|\t\t|\n";
+    cout << "R\t\t|\n";
+    cout << "N\t\t|\n";
+    cout << "W\t\t|\n";
+    cout << "|\t\t|\n";
+    cout << "----------";
+}
