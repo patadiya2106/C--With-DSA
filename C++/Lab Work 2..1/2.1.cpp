04ad30dc56fef7494e37d4dd85a14bf8aea8d85a
Rahul Patadiya <rahulpatadiya07@gmail.com>
2025-05-12 11:54:58 +0530

    12-5-2025

---
+++ C++/Lab Work 2..1/2.1.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+
+using namespace std;
+
+main(){
+    int a = 21,b=12;
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << "Addition of 21 and 12 is 33" << endl << endl;
+ 
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << "Subtraston of 21 and 12 is 9" << endl << endl;
+    
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << "Multiplication  of 21 and 12 is 252" << endl << endl;
+    
+    cout << a << "  / " << b << " = " << a / b << endl;
+    cout << "Division of 21 and 12 is 1" << endl << endl;
+
+    cout << a << "  % " << b << " = " << a % b << endl;
+    cout << "Division of 21 and 12 is 9" << endl;
+     
+}
